dbf1dd0f427c092e4082a9bb59780221cb80f093
kristianjucan <kristian@inntech.dev>
2022-11-29 12:52:49 +0200

    added dynamic switching svgs

--- components/SvgRender.vue/SvgRender.vue
+++ components/SvgRender.vue/SvgRender.vue
@@ -17,6 +17,26 @@
             <path
                 d="M65.9 2.3L82.8 37l38.2 5.3c3.3.5 4.7 4.6 2.2 6.9L95.4 76l6.8 38c.6 3.3-2.9 5.9-5.9 4.3l-34.1-18.2-34.1 18.2c-3 1.6-6.5-1-5.9-4.3L29 76 1.2 49.2c-2.4-2.3-1.1-6.5 2.2-6.9L41.7 37 58.6 2.3c1.5-3.1 5.8-3.1 7.3 0z" />
         </svg>
+        <svg v-if="props.icon == 'ro'" width="20" height="17" fill="currentColor" viewBox="0 0 3 2">
+            <rect width="3" height="2" x="0" y="0" fill="#002B7F"></rect>
+            <rect width="2" height="2" x="1" y="0" fill="#FCD116"></rect>
+            <rect width="1" height="2" x="2" y="0" fill="#CE1126"></rect>
+        </svg>
+        <svg v-if="props.icon == 'en'" width="20" height="20" fill="currentColor" viewBox="0 0 60 30">
+            <clipPath id="s">
+                <path d="M0,0 v30 h60 v-30 z"></path>
+            </clipPath>
+            <clipPath id="t">
+                <path d="M30,15 h30 v15 z v15 h-30 z h-30 v-15 z v-15 h30 z"></path>
+            </clipPath>
+            <g clip-path="url(#s)">
+                <path d="M0,0 v30 h60 v-30 z" fill="#012169"></path>
+                <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" stroke-width="6"></path>
+                <path d="M0,0 L60,30 M60,0 L0,30" clip-path="url(#t)" stroke="#C8102E" stroke-width="4"></path>
+                <path d="M30,0 v30 M0,15 h60" stroke="#fff" stroke-width="10"></path>
+                <path d="M30,0 v30 M0,15 h60" stroke="#C8102E" stroke-width="6"></path>
+            </g>
+        </svg>
     </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,41 @@
 <template>
     <div class="w-full p-10">
-        <!-- Use the SvgRenderer component and pass the icon prop -->
-        <div class="flex gap-10 justify-center text-red-400  ">
-            <!-- If you are using TailwindCSS you can set the properties of the svg in class -->
-            <!-- You can use it like this : <SvgRender class="w-5 sm:w-10 h-5 sm:h-10" :icon="item.icon" /> -->
-            <!-- Or like this : -->
-            <div v-for="(item, index) in arrayOfIcons" v-bind:key="index">
-                <SvgRender :width="20" :height="20" :icon="item.icon" />
+        <div class="flex flex-col  items-center gap-10">
+            <!-- Use the SvgRenderer component and pass the icon prop -->
+            <div class="flex gap-10 justify-center text-red-400  ">
+                <!-- If you are using TailwindCSS you can set the properties of the svg in class -->
+                <!-- You can use it like this : <SvgRender class="w-5 sm:w-10 h-5 sm:h-10" :icon="item.icon" /> -->
+                <!-- Or like this : -->
+                <div v-for="(item, index) in arrayOfIcons" v-bind:key="index">
+                    <SvgRender :width="20" :height="20" :icon="item.icon" />
+                </div>
+            </div>
+            <div class="flex flex-col items-center">
+                <p class="pb-2 font-bold">Example of dynamic switching SVGs:</p>
+                <div class="relative w-60 cursor-pointer group px-5 border-t-[1px]">
+
+                    <div class="flex p-2 justify-between items-center  hover:bg-slate-50 ">
+                        <div class="">
+                            <p>Language:</p>
+                        </div>
+                        <div class="flex gap-1 items-center  px-3 ">
+                            <div class="flex gap-2 items-center">
+                                <p class="text-primary">{{ flags[selectedLanguage].name }}</p>
+                                <SvgRender :icon="flags[selectedLanguage].icon" />
+                            </div>
+                            <div
+                                class="group-hover:visible group-hover:opacity-100 duration-500 opacity-0 absolute invisible left-0 p-5 h-max top-10 w-60 shadow-md ">
+                                <div v-for="(item, index) in flags" v-bind:key="index">
+                                    <div class=" flex justify-between w-full py-2 px-2 hover:bg-slate-50 rounded-lg"
+                                        @click="selectedLanguage = item.id">
+                                        <p class="">{{ item.name }}</p>
+                                        <SvgRender :icon="item.icon" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -17,5 +46,12 @@ const arrayOfIcons = [
     { icon: 'check' },
     { icon: 'briefcase' },
     { icon: 'star' },
-]
+];
+
+const flags = [
+    { icon: 'en', name: 'English', id: 0 },
+    { icon: 'ro', name: 'Romanian', id: 1 },
+];
+
+const selectedLanguage = ref(0)
 </script>
